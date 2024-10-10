34fe652040de07a886493032ca74944ef12756ca
Amelss <ameleyka@gmail.com>
2024-10-10 21:28:20 +0100

    Creating contentful client

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
+import { createClient } from "contentful";
 
-
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  }
+  )
+};
 
 export default function Home() {
   return (
     <div>
-    <h1 className="text-green-500">Hello</h1>
+      <h1 className="text-green-500">Hello</h1>
     </div>
   );
 }
