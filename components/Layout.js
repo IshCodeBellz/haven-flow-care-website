ed98cf44ff28f3379ed0e075a20a6814cf4cb909
Ahmed Adeyemi Bello <ahmbello@hotmail.com>
2024-10-11 01:43:01 +0100

    update contact page

--- components/Layout.js
+++ components/Layout.js
@@ -5,15 +5,12 @@ import Footer from "./Footer";
 export default function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
-    
       <header className="lg:w-[1100px] mx-auto w-full">
         <Header />
       </header>
 
       <main className="flex-grow bg-dustyPink w-full">
-        <div className="lg:w-[1100px] mx-auto w-full ">
-          {children}
-        </div>
+        <div className="lg:w-[1100px] mx-auto w-full ">{children}</div>
       </main>
 
       <footer className="w-full">
