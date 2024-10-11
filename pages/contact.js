6755de603338325a11d247744005b3bebf47556c
Ahmed Adeyemi Bello <ahmbello@hotmail.com>
2024-10-11 12:53:06 +0100

    update contact page

--- pages/contact.js
+++ pages/contact.js
@@ -8,8 +8,6 @@ export default function Contact() {
     phonenumber: "",
     comments: "",
     newsletter: true,
-    employment: "",
-    favColor: "",
   });
 
   const id = React.useId();
@@ -40,76 +38,86 @@ export default function Contact() {
         </div>
       </div>
       <div className="grid grid-cols-2 gap-20 bg-white">
-        <div>
-          <h1 className="pt-10 pl-8 pb-2 text-black text-5xl">Contact Us</h1>
-          <p className="pl-8 text-lg pr-24">
+        <div className="pl-12 text-sm">
+          <h1 className="pt-10 pb-2 text-black text-5xl">Contact Us</h1>
+          <p className="text-lg pr-24">
             For all general inquiries call 07904 333 304 <br /> Or fill in our
             contact form:
           </p>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor={id + "-firstName"}>First Name</label>
-            <br />
-            <input
-              type="text"
-              onChange={handleChange}
-              name="firstName"
-              value={formData.firstName}
-              id={id + "-firstName"}
-            />
-            <br />
-            <label htmlFor={id + "-lastName"}>Last Name</label>
-            <br />
-            <input
-              type="text"
-              onChange={handleChange}
-              name="lastName"
-              value={formData.lastName}
-              id={id + "-lastName"}
-            />
-            <br />
-            <label htmlFor={id + "-email"}>Email</label>
-            <br />
-            <input
-              type="email"
-              onChange={handleChange}
-              name="email"
-              value={formData.email}
-              id={id + "-email"}
-            />
-            <br />
-            <label htmlFor={id + "-phonenumber"}>Phone</label>
-            <br />
-            <input
-              type="phonenumber"
-              onChange={handleChange}
-              name="phonenumber"
-              value={formData.phonenumber}
-              id={id + "-phonenumber"}
-            />
-            <br />
-            <input
-              type="checkbox"
-              id={id + "-newsletter"}
-              checked={formData.newsletter}
-              onChange={handleChange}
-              name="newsletter"
-            />
-            <label htmlFor={id + "-newsletter"}>
-              yes, subscribe me to your newsletter
-            </label>
-            <br />
-            <label htmlFor={id + "-comments"}>Equiry</label>
-            <br />
-            <textarea
-              value={formData.comments}
-              onChange={handleChange}
-              name="comments"
-              id={id + "-comments"}
-            />
-            <br />
-            <br />
-            <button>Submit</button>
-          </form>
+          <div className=" pt-6 pb-12">
+            <form onSubmit={handleSubmit}>
+              <label htmlFor={id + "-firstName"}>First Name</label>
+              <br />
+              <input
+                className="p-2 mt-2 mb-4 border-black-500/50 border-2 focus:outline-none focus:border-sky-500/50  focus:ring-1 w-10/12"
+                type="text"
+                onChange={handleChange}
+                name="firstName"
+                value={formData.firstName}
+                id={id + "-firstName"}
+              />
+              <br />
+              <label htmlFor={id + "-lastName"}>Last Name</label>
+              <br />
+              <input
+                className="p-2 mt-2 mb-4 border-black-500/50 border-2 focus:outline-none focus:border-sky-500/50  focus:ring-1 w-10/12"
+                type="text"
+                onChange={handleChange}
+                name="lastName"
+                value={formData.lastName}
+                id={id + "-lastName"}
+              />
+              <br />
+              <label htmlFor={id + "-email"}>Email</label>
+              <br />
+              <input
+                className="p-2 mt-2 mb-4 border-black-500/50 border-2 focus:outline-none focus:border-sky-500/50  focus:ring-1 w-10/12"
+                type="email"
+                onChange={handleChange}
+                name="email"
+                value={formData.email}
+                id={id + "-email"}
+              />
+              <br />
+              <label htmlFor={id + "-phonenumber"}>Phone</label>
+              <br />
+              <input
+                className="p-2 mt-2 mb-4 border-black-500/50 border-2 focus:outline-none focus:border-sky-500/50  focus:ring-1 w-10/12"
+                type="phonenumber"
+                onChange={handleChange}
+                name="phonenumber"
+                value={formData.phonenumber}
+                id={id + "-phonenumber"}
+              />
+              <br />
+              <input
+                className="mt-2 mb-4 indeterminate:bg-gray-300 checked:bg-slate-500"
+                type="checkbox"
+                id={id + "-newsletter"}
+                checked={formData.newsletter}
+                onChange={handleChange}
+                name="newsletter"
+              />
+              <label htmlFor={id + "-newsletter"} className="pl-2">
+                Yes, subscribe me to your newsletter.
+              </label>
+              <br />
+              <label htmlFor={id + "-comments"}>Equiry</label>
+              <br />
+              <textarea
+                className="p-2 mt-2 mb-4 border-black-500/50 border-2 focus:outline-none focus:border-sky-500/50  focus:ring-1 w-10/12"
+                value={formData.comments}
+                onChange={handleChange}
+                name="comments"
+                id={id + "-comments"}
+              />
+              <br />
+              <br />
+              <button className="rounded bg-panelBlue w-10/12 p-2 text-white">
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
         <div>
           <p className="pt-24">
