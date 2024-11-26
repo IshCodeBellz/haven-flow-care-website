e3479a675268a4f64730fa3cb06ff32a3f4efbb1
Amelss <ameleyka@gmail.com>
2024-11-26 23:30:59 +0000

    Removal of file

--- utils/contentful.js
+++
@@ -1,6 +0,0 @@
-import * as contentful from "contentful";
-
-export const client = contentful.createClient({
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_KEY,
-  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-});
